bdb216af0404bc8c6d5377c6afa1b62eec2e7a3b
VitaliiArtemenko <vitaliiart1977@gmail.com>
2022-04-30 22:09:46 -0700

    Data Reverse - 7 kuy

--- Tasks/Kuy6/DataReverse/dataReverse.js
+++ Tasks/Kuy6/DataReverse/dataReverse.js
@@ -2,7 +2,7 @@ function dataReverse(data) {
     const result = [];
 
     for (let i = 0; i < data.length; i+=8) {
-        result.push(data.slice(0 + i, 8 + i));
+        result.push(data.slice(i, 8 + i));
     }
     result.reverse();
     return [].concat(...result);
