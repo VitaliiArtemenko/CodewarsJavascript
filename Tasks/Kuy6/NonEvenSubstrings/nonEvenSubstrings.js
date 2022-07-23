46c6c49cecdbc3875dc1c24e6329af642aa2b7a4
VitaliiArtemenko <vitaliiart1977@gmail.com>
2022-07-22 22:09:10 -0700

    Non-even substrings - kuy 6

--- Tasks/Kuy6/NonEvenSubstrings/nonEvenSubstrings.js
+++ Tasks/Kuy6/NonEvenSubstrings/nonEvenSubstrings.js
@@ -1,18 +1,5 @@
 function nonEvenSubstrings(s) {
-    let nums = []
-
-    for (let i = 0; i < s.length; i++) {
-        for (let j = i + 1; j <= s.length; j++) {
-            if (parseInt(s.slice(i, j)) % 2 === 1) {
-                nums.push(s.slice(i, j))
-            }
-        }
-    }
-    return nums.length;
+    return [...s].reduce((curr, el, index) => curr + (+el % 2 === 1 ? index + 1 : 0), 0);
 }
 
-let str = '9163241925954926243'
-
-console.log(nonEvenSubstrings(str))
-
-// TODO: https://www.codewars.com/kata/59da47fa27ee00a8b90000b4/train/javascript
+module.exports = nonEvenSubstrings;
